fe7538ba661785621c0d3d6eb0bf5e75e8a2d3b4
Stan Guner <guner.stan@gmail.com>
2025-02-17 20:54:09 +0000

    move old deploy to exampled

--- deploy/examples/fetch_yahoo_data_ecs_fargate.py
+++ deploy/examples/fetch_yahoo_data_ecs_fargate.py
@@ -1,12 +1,14 @@
 from acme_prefect.flows.fetch_yahoo_data import main
 
-# TODO: use https://docs-3.prefect.io/v3/deploy/infrastructure-concepts/prefect-yaml
 if __name__ == "__main__":
     main.deploy(
         name="fetch-yahoo-data-ecs-fargate",
         work_pool_name="ecs-pool",
         cron="0 12 * * 1-5",
         image="ghcr.io/blackwhitehere/acme-prefect:latest", # we should specify exact version here
+        # todo: set from acme_config
+        job_variables={"env": {}},
+        tags=[],
         build=False,
         push=False,
     )
