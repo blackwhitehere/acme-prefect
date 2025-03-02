eb2d6bfffa40cc8c722fddd7bfecae0a2838df8f
Stan Guner <guner.stan@gmail.com>
2025-03-02 10:51:33 +0000

    add main section

--- src/acme_prefect/flows/example.py
+++ src/acme_prefect/flows/example.py
@@ -3,3 +3,6 @@ from prefect import flow
 @flow(log_prints=True, description="An example flow")
 def example_flow():
     print("I'm an example flow!")
+
+if __name__ == "__main__":
+    example_flow()
