063770ffd9579ce87d64fd349105738490c99675
Stan Guner <guner.stan@gmail.com>
2025-03-02 11:30:23 +0000

    removed for demo

--- src/acme_prefect/flows/welcome.py
+++
@@ -1,8 +0,0 @@
-from prefect import flow
-
-@flow(log_prints=True, description="A welcome flow")
-def welcome_flow():
-    print("I'm a welcome flow!")
-
-if __name__ == "__main__":
-    welcome_flow()
