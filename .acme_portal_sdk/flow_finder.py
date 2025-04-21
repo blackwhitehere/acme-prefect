30f71f5b0653ed379f5886263879d161ca6ac8f5
Stan Guner <guner.stan@gmail.com>
2025-04-21 20:04:14 +0200

    adjust to use acme_portal_sdk

---
+++ .acme_portal_sdk/flow_finder.py
@@ -0,0 +1,8 @@
+from acme_portal_sdk.prefect.flow_finder import PrefectFlowFinder, PrefectFlowDetails
+from pathlib import Path
+
+# Create an instance of PrefectFlowFinder
+project_root = Path(__file__).parent.parent
+flow_finder = PrefectFlowFinder(
+    root_dir=str(project_root / "src" / "acme_prefect" / "flows")
+)
