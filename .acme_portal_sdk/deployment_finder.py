8c82575757a3f95cdc8fbbde864d379205837668
Stan Guner <guner.stan@gmail.com>
2025-04-25 13:55:51 +0100

    debug changes

--- .acme_portal_sdk/deployment_finder.py
+++ .acme_portal_sdk/deployment_finder.py
@@ -2,3 +2,8 @@ from acme_portal_sdk.prefect.deployment_finder import PrefectDeploymentFinder
 
 # Will require prefect client be authenticated against prefect server like prefect cloud
 deployment_finder = PrefectDeploymentFinder()
+
+if __name__ == "__main__":
+    from pprint import pprint
+    deployments = deployment_finder.get_deployments()
+    pprint(deployments)
