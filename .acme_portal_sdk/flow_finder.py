fe556584da8d0afa85f10472e53ee76648d0c750
Stan Guner <guner.stan@gmail.com>
2025-04-25 14:25:52 +0100

    fix for flow discovery

--- .acme_portal_sdk/flow_finder.py
+++ .acme_portal_sdk/flow_finder.py
@@ -4,5 +4,5 @@ from pathlib import Path
 # Create an instance of PrefectFlowFinder
 project_root = Path(__file__).parent.parent
 flow_finder = PrefectFlowFinder(
-    root_dir=str(project_root / "src" / "acme_prefect" / "flows")
+    root_dir=str(project_root / "src" / "acme_prefect")
 )
