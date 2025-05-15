1e241d3c66cf4106a63abad4a4447104827f4e69
Stan Guner <guner.stan@gmail.com>
2025-05-15 17:24:54 +0100

    new comment

--- src/acme_prefect/flows/subflows/sample_two.py
+++ src/acme_prefect/flows/subflows/sample_two.py
@@ -2,7 +2,7 @@ from prefect import flow
 
 @flow(log_prints=True, description="A sample flow")
 def sample_two():
-    # sample comment
+    # sample comment -- new comment
     print("I'm an example flow!")
 
 if __name__ == "__main__":
