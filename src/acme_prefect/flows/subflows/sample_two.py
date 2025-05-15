2d8483bf179589f4809d2dfd2fa1b0ae36e6e257
Stan Guner <guner.stan@gmail.com>
2025-05-15 17:11:31 +0100

    new flow

---
+++ src/acme_prefect/flows/subflows/sample_two.py
@@ -0,0 +1,9 @@
+from prefect import flow
+
+@flow(log_prints=True, description="A sample flow")
+def sample_two():
+    # sample comment
+    print("I'm an example flow!")
+
+if __name__ == "__main__":
+    sample_two()
